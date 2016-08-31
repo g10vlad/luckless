3e6c6de6ed7796bd7c223df423ac911303df7045
Vladimir <Vladimir>
2016-08-30 23:00:58 -0500

    Re-designed auth api (login)

--- auth_web/auth_web_apps/access/services/loginService.js
+++ auth_web/auth_web_apps/access/services/loginService.js
@@ -64,3 +64,10 @@
         saveRegistration: "_saveRegistration",
     };
 });
+$('#login-button').click(function(){
+  $('#login-button').fadeOut("slow",function(){
+    $("#container").fadeIn();
+    TweenMax.from("#container", .4, { scale: 0, ease:Sine.easeInOut});
+    TweenMax.to("#container", .4, { scale: 1, ease:Sine.easeInOut});
+  });
+});
